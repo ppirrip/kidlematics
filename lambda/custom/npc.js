39f61da6dd3571957e0b197258903ff7545691c7
Pip Pirrip <ppirrip@gmail.com>
2017-12-13 11:35:27 -0500

    minor refactoring of the code

---
+++ lambda/custom/npc.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const Boko_Dialog = {
+    'DESC': [],
+    'INTRO': [
+        'welcome stranger, we need your help',
+        'our town treasure has been stolen, please take it back for us',
+        'it is taken to the old castle ruin',
+        'do you know where to go'
+    ],
+    'MORE': [
+        'please bring back our town treasure, it is very important to us'
+    ],
+    'LAST': [
+        'please bring back our town treasure, it is very important to us'
+    ]
+};
+
+const Sozea_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Dihpaz_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Jia_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Munden_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Troll_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Bear_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Bandit_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+const Grinchearna_Dialog = {
+    'DESC': [],
+    'INTRO': [],
+    'MORE': [],
+    'LAST': []
+};
+
+module.exports = {
+    'NPC_LIST': {
+        'boko': Boko_Dialog, 
+        'sozea': Sozea_Dialog, 
+        'dihpaz': Dihpaz_Dialog,
+        'jia': Jia_Dialog, 
+        'munden': Munden_Dialog, 
+        'troll': Troll_Dialog, 
+        'bear': Bear_Dialog, 
+        'bandit': Bandit_Dialog,
+        'grinchearna': Grinchearna_Dialog
+    }
+}
---
+++ lambda/custom/setting.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+    'COUNTRY_NAME': 'Kidlematica',
+    'PUB_NAME': 'Blind Duck Pub',
+    'FINAL_SCENE': 'Castle Ruin',
+    'WAY_POINT_1': [
+        {
+            'LOCATION': 'bridge',
+            'CHALLENGE': 'troll',
+            'DESC': ['A Troll is guarding a bridge where you have to cross'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+        {
+            'LOCATION': 'cave',
+            'DESC': ['A Bear is sleeping at the openning of a cave where you need to enter'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+        {
+            'LOCATION': 'freeway',
+            'DESC': ['A bandit is guarding a blockade on the free way'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+    ],
+    'WAY_POINT_2': [
+        {
+            'LOCATION': 'forrest',
+            'DESC': ['You are in a forrest crossing where to have to decide going left, or right'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+        {
+            'LOCATION': 'underground maze',
+            'DESC': ['You are inside an underground maze where to have to decide take the left, or right'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+        {
+            'LOCATION': 'ancient city ruin',
+            'DESC': ['You are inside an ancient city ruin where to have to decide going left, or right'],
+            'INTRO': [],
+            'MORE': [],
+            'LAST': []
+        },
+    ]
+}
